b25e7c904cd8ba7e6d63a0b9a0e23eb8817513c0
Blueve <672454911@qq.com>
2016-07-03 19:16:25 +0800

    Add a abstract container class
    A container is indicate a block of memory resource

---
+++ NodeManager/Container.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyRM.NodeManager
+{
+    abstract class Container : IDisposable
+    {
+        #region Public Property
+        /// <summary>
+        /// Memory limit of this container
+        /// </summary>
+        public int MemoryLimit { get; private set; }
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Init a container with a memory limit
+        /// </summary>
+        /// <param name="memory">Memory limit (MB)</param>
+        protected Container(int memory)
+        {
+            MemoryLimit = memory;
+        }
+        #endregion
+
+        #region Public Abstract Method
+        /// <summary>
+        /// Get current memory usage of this container
+        /// </summary>
+        /// <returns>Memory usage (MB)</returns>
+        public abstract long GetMemoryUsage();
+
+        /// <summary>
+        /// Release resources
+        /// </summary>
+        public abstract void Dispose();
+        #endregion
+    }
+}
