e665564a3fc13c0eaa240f61485399cd6ff0d34f
Blueve <672454911@qq.com>
2016-07-06 23:04:48 +0800

    New project for test WinApplication container

---
+++ SampleWorkload/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleWorkload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length != 1)
+            {
+                return;
+            }
+            long maxItem;
+            try
+            {
+                maxItem = long.Parse(args[0]) * 1024 * 1024 / 8;
+
+                var list = new List<Int64>();
+                for(int i = 0; i < maxItem; i++)
+                {
+                    list.Add(1L);
+                }
+            }
+            catch(Exception)
+            {
+                return;
+            }
+        }
+    }
+}
