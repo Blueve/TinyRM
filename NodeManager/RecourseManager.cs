17b9e1ba8edd7f96c249e27742b22fecfc135b8d
Blueve <672454911@qq.com>
2016-07-12 20:09:41 +0800

    Add singleton class RecourseManager

---
+++ NodeManager/RecourseManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyRM.NodeManager
+{
+    class RecourseManager
+    {
+        private static Lazy<RecourseManager> _instance = new Lazy<RecourseManager>(() => new RecourseManager());
+
+        private readonly HashSet<ContainerManager> _containers = new HashSet<ContainerManager>();
+
+        public RecourseManager Instance
+        {
+            get { return _instance.Value; }
+        }
+    }
+}
