f008a061fe1e0ca5b087450cf43a09cc70183023
Blueve <672454911@qq.com>
2016-07-12 20:09:17 +0800

    Add container factory

---
+++ NodeManager/ContainerFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyRM.NodeManager
+{
+    public static class ContainerFactory
+    {
+        public static ContainerManager CreateWinContainer(int memoryLimit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
